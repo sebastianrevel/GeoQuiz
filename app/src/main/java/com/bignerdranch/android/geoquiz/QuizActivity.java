8a4314d22fb71bfcb58afa217722a485eff456d3
Sebastian Revel <sebastianrevel@college.harvard.edu>
2018-07-03 19:18:20 -0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                toast = (Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT));
+                toast.setGravity(Gravity.TOP, 0,0);
+                toast.show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast = (Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT));
+                toast.setGravity(Gravity.TOP, 0,0);
+                toast.show();
+            }
+        });
+    }
+}
