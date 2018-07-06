d96cbbebe834f56ad3b5bcd659cad52ea4771b44
Sebastian Revel <sebastianrevel@college.harvard.edu>
2018-07-05 21:42:26 -0700

    Added a score counter and orientation support

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,9 @@ public class QuizActivity extends AppCompatActivity {
     private Button mNextButton;
     private TextView mQuestionTextView;
     private Toast toast;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final String OUT_OF_7 = "/7 correct!";
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
@@ -26,12 +30,17 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private int mCurrentScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
+        Log.d(TAG, "onCreate(Bundle) called");
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mCurrentScore = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
@@ -39,6 +48,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
+                mFalseButton.setVisibility(View.GONE);
+                mTrueButton.setVisibility(View.GONE);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -46,6 +57,9 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
+                mFalseButton.setVisibility(View.GONE);
+                mTrueButton.setVisibility(View.GONE);
+
             }
         });
 
@@ -55,10 +69,47 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+                mFalseButton.setVisibility(View.VISIBLE);
+                mTrueButton.setVisibility(View.VISIBLE);
+                if (mCurrentIndex == 5){
+                    showScore();
+                }
             }
         });
         updateQuestion();
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -69,11 +120,29 @@ public class QuizActivity extends AppCompatActivity {
         int messageResId = 0;
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
+            mCurrentScore++;
         } else {
             messageResId = R.string.incorrect_toast;
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+    public void showScore() {
+        if (mCurrentScore == 0) {
+            Toast.makeText(this, R.string.score0, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 1) {
+            Toast.makeText(this, R.string.score1, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 2) {
+            Toast.makeText(this, R.string.score2, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 3) {
+            Toast.makeText(this, R.string.score3, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 4) {
+            Toast.makeText(this, R.string.score4, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 5) {
+            Toast.makeText(this, R.string.score5, Toast.LENGTH_SHORT).show();
+        } else if (mCurrentScore == 6) {
+            Toast.makeText(this, R.string.score6, Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
